0dad95edf35cfdb4847dc04316b37da500bc0a50
spolli <kebabnopiccante@gmail.com>
2019-11-04 15:43:53 +0100

    add progress bar

--- TelegramApi.py
+++ TelegramApi.py
@@ -1,33 +1,41 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from telethon.sync import TelegramClient
+from telethon import TelegramClient
+import os
+from progress.bar import Bar
 from src.config import API_ID, API_HASH, SESSION_NAME, TOKEN_API
 
 bot = TelegramClient(SESSION_NAME, API_ID, API_HASH).start(bot_token=TOKEN_API)
 
 #client.send_message('spolli', 'Hello! Talking to you from Telethon')
 
+
 async def main():
-    print('Start Uploading...')
+    file_size = os.path.getsize('./src/video/Kandagawa-Jet-Girls.mp4') / 512
+    prog_bar = Bar('\t\tUploading: File\t\t', max=file_size)
     file_id = await bot.send_file(
-        entity='spolli', 
-        file=open('./src/video/BokutachiWaBenkyouGaDekinai.mp4', 'rb'), 
-        caption='Sample', 
-        supports_streaming=True
+        entity='spolli',
+        file=open('./src/video/Kandagawa-Jet-Girls.mp4', 'rb'),
+        caption='Sample',
+        supports_streaming=True,
+        part_size_kb=512,
+        progress_callback=prog_bar.next()
     )
+    prog_bar.finish()
     print('Finish Uploading...')
     print(file_id.stringify())
+
 
 async def main2():
     print('Start Uploading...')
     file_id = await bot.upload_file(
-        file=open('./src/video/BokutachiWaBenkyouGaDekinai.mp4', 'rb'),
-        part_size_kb=512,
+        file=open('./src/video/Kandagawa-Jet-Girls.mp4', 'rb'),
+
         file_name='sample'
     )
     print('Finish Uploading...')
     print(file_id.stringify())
 
 with bot:
-    bot.loop.run_until_complete(main2())
+    bot.loop.run_until_complete(main())
--- main.py
+++ main.py
@@ -9,28 +9,32 @@ from telegram.ext import Updater, CommandHandler
 import logging
 import time
 import os
+from progress.bar import Bar
 
-folder_path={
+folder_path = {
     'video': './src/video'
 }
 db = sqliteDriver()
 
+
 def delete_file(filename):
     try:
-        if os.path.isfile(file_path):
-            os.unlink(file_path)
+        if os.path.isfile(filename):
+            os.unlink(filename)
     except Exception as e:
         print(e)
+
 
 def send_video_telegram(context, update, episode, filename):
     print('Sending video to Telegram...')
     context.bot.send_video(
-        chat_id=update.message.chat.id, 
+        chat_id=update.message.chat.id,
         video=open(filename, 'rb'),
         caption=f'#{episode.name} | Episode: {episode.number}',
         timeout=30000,
         supports_streaming=True
     )
+
 
 def download_video(context, update, episode):
     if not episode.link is None:
@@ -38,15 +42,21 @@ def download_video(context, update, episode):
         name = '-'.join(episode.name.split(' '))
         filename = f'{folder_path["video"]}/{name}.mp4'
         if not os.path.isfile(filename):
-            chunk_size = 256
+            chunk_size = 512
             r = requests.get(url, stream=True)
+            file_len = int(requests.head(
+                url).headers['content-length']) / chunk_size
             with open(filename, "wb") as f:
-                print('Downloading Video...')
+                prog_bar = Bar(
+                    f'\t\tDownloading: {episode.name}\t\t', max=file_len)
                 for chunk in r.iter_content(chunk_size=chunk_size):
                     f.write(chunk)
+                    prog_bar.next()
+                prog_bar.finish()
         send_video_telegram(context, update, episode, filename)
         db.add_record(episode)
         delete_file(filename)
+
 
 def start(update, context):
     print('Start main')
@@ -57,9 +67,9 @@ def start(update, context):
             if anime.episode < 30:
                 last_episode = anime._get_last_episode()
                 if db.find_record(last_episode) is None:
-                    print(f'Downloading: {last_episode.name}')
                     download_video(context, update, last_episode)
         time.sleep(TIME_INTERVAL)
+
 
 def animelist(update, context):
     rows = db.get_animeList()
@@ -72,21 +82,23 @@ def animelist(update, context):
         msg = 'Nessun anime trovato'
     update.message.reply_text(msg)
 
+
 def main():
-    updater = Updater(token=TOKEN_API, use_context=True, request_kwargs={'read_timeout': 50, 'connect_timeout': 50})
+    updater = Updater(token=TOKEN_API, use_context=True, request_kwargs={
+        'read_timeout': 50, 'connect_timeout': 50})
     print('Starting telegram bot...')
     dp = updater.dispatcher
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
+                        level=logging.INFO)
     dp.add_handler(CommandHandler("start", start, pass_args=False))
     dp.add_handler(CommandHandler("animelist", animelist, pass_args=False))
     # Start the Bot
     updater.start_polling()
     print('Telegram bot start')
-    #updater.start_webhook(listen='127.0.0.1', port=5002, url_path=TOKEN_API)
+    # updater.start_webhook(listen='127.0.0.1', port=5002, url_path=TOKEN_API)
     updater.idle()
 
-if __name__ == '__main__':
-    main()
-    
 
+if __name__ == '__main__':
+    start(None, None)
+    # main()
